520c6a620014ac3ad3cbcba4f1fb64e072e86875
Jan-Oliver Brandt <jan-oliver.brandt@btc-ag.com>
2023-08-17 14:08:27 +0200

    Remove Transparent Plugin for perfomance

--- init.lua
+++ init.lua
@@ -278,39 +278,39 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'xiyaowong/transparent.nvim',
-    config = function()
-      require('transparent').setup {
-        groups = { -- table: default groups
-          'Normal',
-          'NormalNC',
-          'Comment',
-          'Constant',
-          'Special',
-          'Identifier',
-          'Statement',
-          'PreProc',
-          'Type',
-          'Underlined',
-          'Todo',
-          'String',
-          'Function',
-          'Conditional',
-          'Repeat',
-          'Operator',
-          'Structure',
-          'LineNr',
-          'NonText',
-          'SignColumn',
-          'CursorLineNr',
-          'EndOfBuffer',
-        },
-        extra_groups = { 'Neotree' },       -- table: additional groups that should be cleared
-        exclude_groups = { 'NormalFloat' }, -- table: groups you don't want to clear
-      }
-    end,
-  },
+  -- {
+  --   'xiyaowong/transparent.nvim',
+  --   config = function()
+  --     require('transparent').setup {
+  --       groups = { -- table: default groups
+  --         'Normal',
+  --         'NormalNC',
+  --         'Comment',
+  --         'Constant',
+  --         'Special',
+  --         'Identifier',
+  --         'Statement',
+  --         'PreProc',
+  --         'Type',
+  --         'Underlined',
+  --         'Todo',
+  --         'String',
+  --         'Function',
+  --         'Conditional',
+  --         'Repeat',
+  --         'Operator',
+  --         'Structure',
+  --         'LineNr',
+  --         'NonText',
+  --         'SignColumn',
+  --         'CursorLineNr',
+  --         'EndOfBuffer',
+  --       },
+  --       extra_groups = { 'Neotree' },       -- table: additional groups that should be cleared
+  --       exclude_groups = { 'NormalFloat' }, -- table: groups you don't want to clear
+  --     }
+  --   end,
+  -- },
 }, {})
 
 -- vim.keymap.set('n', '<leader>gg>', 'LazyGit<cr>')
